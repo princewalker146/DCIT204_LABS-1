b423f78a3458548d526b5155dadbac65742505e2
Prince Walker <princeantwiwalker15@gmail.com>
2026-06-15 21:31:38 +0000

    Completed lab1 assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
 
+        int target = 30;
+
+        int result = Algorithms.linearSearch(array, target);
+
+        System.out.println("Index found: " + result);
+        int binaryResult = Algorithms.binarySearch(array, target);
+        System.out.println("Binary Search Index: " + binaryResult);
     }
 }
